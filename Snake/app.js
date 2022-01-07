073227c65bccefc5892f67e992d7840b732a44ad
Katarzyna Kiryłowicz <k.kirylowicz@gmail.com>
2022-01-07 19:59:27 +0100

    Update snake crash logic

--- Snake/app.js
+++ Snake/app.js
@@ -7,7 +7,7 @@ const ARROW_RIGHT = 39;
 const ARROW_DOWN = 40;
 
 let directions = [1, 0];
-let gameSpeed = 500;
+let gameSpeed = 200;
 let gameInterval = 0;
 let score = 0;
 let live = 3;
@@ -25,27 +25,76 @@ const INITIAL_SNAKE = [
     [50, 30],
     [49, 30],
     [48, 30],
+    [47, 30],
+    [46, 30],
+    [45, 30],
+    [43, 30],
+    [42, 30]
 ];
 
 let snake = [...INITIAL_SNAKE];
 
+
+let newPlant = [];
+
+const renderPlants = () => {
+    let plantsCords = [];
+
+    for (let i = 0; i < 5; i++) {
+        xplant = Math.floor(Math.random() * w);
+        yplant = Math.floor(Math.random() * h);
+        newPlant.push(`<div class="plants" style="top: ${yplant * 10}px; left: ${xplant * 10}px;"></div>`);
+        plantsCords.push([xplant, yplant]);
+    }
+
+
+    console.log(`Plants CordS: ${plantsCords}`);
+    console.log(newPlant);
+}
+
 const render = () => {
+
     const plant = `<div class="plants" style="top: ${yplant * 10}px; left: ${xplant * 10}px;"></div>`;
+
+
 
     main.innerHTML = snake.map((coords) => {
         const [x, y] = coords;
         return `<div class="snake" style="top: ${y * 10}px; left: ${x * 10}px;"></div>`;
-    }).join('') + plant;
+    }).join('');
+
+    for (let i = 0; i < 4; i++) {
+        main.innerHTML += newPlant[i];
+    }
+
 };
 
 const endGame = () => {
+    render();
     clearInterval(gameInterval);
     gameInterval = 0;
+    gameSpeed = 200;
 };
 
 const renderLives = () => {
     const liveDisplay = document.querySelector(".livePoints");
     liveDisplay.innerHTML = Array(live).fill(`<div>&hearts;</div>`).join('');
+};
+
+const hasCrashedWithBoard = (newCoords) => {
+    return newCoords[0] === Math.floor(w) ||
+        newCoords[0] === -1 ||
+        newCoords[1] === -1 ||
+        newCoords[1] === Math.floor(h)
+};
+
+const hasCrashedWithHimself = (coords, snake) => {
+    for (let i = snake.length - 1; i > -1; i--) {
+        if (snake[i][0] === coords[0] && snake[i][1] === coords[1]) {
+            return true;
+        }
+    }
+    return false
 };
 
 const processGameFrame = () => {
@@ -55,26 +104,29 @@ const processGameFrame = () => {
     const [deltaX, deltaY] = directions;
 
     const newCoords = [x + deltaX, y + deltaY];
+    console.log(`snake[0]: ${snake[0]},  newCoords:${newCoords}`);
 
     // snake crashes
     if (
-        newCoords[0] === Math.floor(w) ||
-        newCoords[0] === -1 ||
-        newCoords[1] === -1 ||
-        newCoords[1] === Math.floor(h)
+        //crashes with board
+        hasCrashedWithBoard(newCoords) ||
 
+        //crash with himself Check is it works??
+        hasCrashedWithHimself(newCoords, snake)
     ) {
         live--;
 
         // restart
         if (live === 0) {
             renderLives();
-            alert('Game Over!')
             endGame();
+            setTimeout(() => alert('Game Over!'));
 
             live = 3;
             points = 0;
+            gameSpeed = gameSpeed + 5;
             snake = [...INITIAL_SNAKE];
+            console.log(`The speed of the snake drops. Now is:${gameSpeed}`);
             return;
         }
 
@@ -87,6 +139,7 @@ const processGameFrame = () => {
     console.log(newCoords, xplant, yplant);
 
     // snake eats plant
+
     if (
         newCoords[0] === xplant &&
         newCoords[1] === yplant
@@ -97,7 +150,9 @@ const processGameFrame = () => {
         score++;
         const points = document.querySelector("span");
         points.innerHTML = `<span>${score}</span>`;
-
+        gameSpeed = gameSpeed - 10;
+        console.log(`Speed of the snake increasses. Now is: ${gameSpeed}`);
+        console.log(`Snake cords are: ${snake}`);
         return;
     }
 
@@ -111,6 +166,7 @@ const startGame = () => {
         return;
     }
     renderLives();
+    renderPlants();
     gameInterval = setInterval(processGameFrame, gameSpeed);
 };
 
@@ -121,7 +177,7 @@ const handleKeyUp = (e) => {
         case ARROW_UP:
             directions = [0, -1];
             break;
-            //strzałka w dół - idzie w dol
+        //strzałka w dół - idzie w dol
         case ARROW_DOWN:
             directions = [0, 1];
             break;
@@ -129,20 +185,20 @@ const handleKeyUp = (e) => {
             // idzie w lewo
             directions = [-1, 0];
             break;
-            //strzałka w prawo - idzie w prawo
+        //strzałka w prawo - idzie w prawo
         case ARROW_RIGHT:
             directions = [1, 0];
             break;
-            // enter - start
+        // enter - start
         case ENTER:
             startGame();
             break;
-            // esc - koniec gry
+        // esc - koniec gry
         case ESC:
             endGame();
             break;
         default:
-            // void
+        // void
     }
 };
 
